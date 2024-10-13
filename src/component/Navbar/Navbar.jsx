d42c3cfc8b940558a3965dd5105e476105d1a8aa
Avinash <akgpcamp@gmail.com>
2024-10-13 20:07:49 +0530

    Profile and ProfileNavigation page added

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import { Avatar, Badge, IconButton } from "@mui/material";
+import {Avatar, Badge, Box, IconButton} from "@mui/material";
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { pink } from "@mui/material/colors";
+import {pink} from "@mui/material/colors";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./Navbar.css";
 
 export const Navbar = () => {
   return (
-    <div className="px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
+      <Box className="px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
       <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
         <li className="logo font-semibold text-gray-300 text-2xl">ZOMATO</li>
       </div>
@@ -30,6 +30,6 @@ export const Navbar = () => {
           </IconButton>
         </div>
       </div>
-    </div>
+      </Box>
   );
 };
